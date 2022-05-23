61464f2ae80cfa592d27d76895e616a1b9fc5cbe
JLGGG <blockboy2957@gmail.com>
2022-05-23 20:08:59 +0900

    feat: add async

--- test.cpp
+++ test.cpp
@@ -5,11 +5,35 @@
 #include <asio/ts/buffer.hpp>
 #include <asio/ts/internet.hpp>
 
+std::vector<char> vBuffer(20 * 1024);
+
+void GrabSomeData(asio::ip::tcp::socket& socket)
+{
+    socket.async_read_some(asio::buffer(vBuffer.data(), vBuffer.size()),
+        [&](std::error_code ec, std::size_t length)
+        {
+            if(!ec)
+            {
+                std::cout << "\n\nRead " << length << "bytes\n\n";
+
+                for (int i = 0; i < length; i++)
+                    std::cout << vBuffer[i];
+
+                GrabSomeData(socket);
+            }
+        }
+    );
+}
+
 int main()
 {
     asio::error_code ec;
     
     asio::io_context context;
+
+    asio::io_context::work idleWork(context);
+
+    std::thread thrContext = std::thread([&]() {context.run();});
 
     // In case using localhost on MAC, you have to run this "sudo apachectl start"
     asio::ip::tcp::endpoint endpoint(asio::ip::make_address("127.0.0.1", ec), 80);
@@ -29,6 +53,8 @@ int main()
 
     if (socket.is_open())
     {
+        GrabSomeData(socket);
+
         std::string sRequest = 
             "GET /index.html HTTP/1.1\r\n"
             "Host: example.com\r\n"
@@ -36,19 +62,12 @@ int main()
 
         socket.write_some(asio::buffer(sRequest.data(), sRequest.size()), ec);
 
-        socket.wait(socket.wait_read);
+        using namespace std::chrono_literals;
+        std::this_thread::sleep_for(2000ms);
 
-        size_t bytes = socket.available();
-        std::cout << "Bytes Available: " << bytes << std::endl;
-
-        if (bytes > 0)
-        {
-            std::vector<char> vBuffer(bytes);
-            socket.read_some(asio::buffer(vBuffer.data(), vBuffer.size()), ec);
-
-            for (auto c : vBuffer)
-                std::cout << c;
-        }
+        context.stop();
+        if (thrContext.joinable()) thrContext.join();
+        
     }
 
     system("pause");
